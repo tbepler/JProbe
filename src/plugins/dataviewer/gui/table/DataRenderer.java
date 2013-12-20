33a985c476bcaa9df6988a85498f7352da967c0a
Tristan Bepler <tbepler@gmail.com>
2013-12-20 12:39:33 -0500

    Number fields are now right aligned while general DataFields are left aligned

--- src/plugins/dataviewer/gui/table/DataRenderer.java
+++ src/plugins/dataviewer/gui/table/DataRenderer.java
@@ -2,10 +2,13 @@ package plugins.dataviewer.gui.table;
 
 import java.awt.Component;
 
+import javax.swing.JLabel;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
 
 import jprobe.services.DataField;
+import jprobe.services.DoubleField;
+import jprobe.services.IntegerField;
 
 class DataRenderer extends DefaultTableCellRenderer{
 	private static final long serialVersionUID = 1L;
@@ -13,7 +16,6 @@ class DataRenderer extends DefaultTableCellRenderer{
 	
 	public DataRenderer(){
 		super();
-		
 	}
 	
 	@Override
@@ -31,7 +33,13 @@ class DataRenderer extends DefaultTableCellRenderer{
 			this.setToolTipText((field == null)?"":field.getTooltip());
 			//return super.getTableCellRendererComponent(table, field.asString(), isSelected, hasFocus, row, column);
 		}
-		return super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+		JLabel render = (JLabel) super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+		if(value instanceof DoubleField || value instanceof IntegerField){
+			render.setHorizontalAlignment(JLabel.RIGHT);
+		}else{
+			render.setHorizontalAlignment(JLabel.LEFT);
+		}
+		return render;
 	}
 	
 }
