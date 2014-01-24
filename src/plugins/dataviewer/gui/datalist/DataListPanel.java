c428c77e46c72c60232a4fd32911f5bc650baad3
Tristan Bepler <tbepler@gmail.com>
2014-01-24 14:06:44 -0500

    Deleted unused methods in DataListPanel

--- src/plugins/dataviewer/gui/datalist/DataListPanel.java
+++ src/plugins/dataviewer/gui/datalist/DataListPanel.java
@@ -1,87 +1,31 @@
 package plugins.dataviewer.gui.datalist;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.GridLayout;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.swing.BorderFactory;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JTable;
 import javax.swing.border.TitledBorder;
-import javax.swing.table.DefaultTableModel;
 
-import plugins.dataviewer.gui.Constants;
 import plugins.dataviewer.gui.DataTabPane;
-import jprobe.services.CoreEvent;
-import jprobe.services.CoreListener;
 import jprobe.services.JProbeCore;
-import jprobe.services.data.Data;
 
 public class DataListPanel extends JPanel{
 	private static final long serialVersionUID = 1L;
-	
-	private DataTabPane m_TabPane;
-	//private JPanel m_InnerPanel;
+
 	private DataListTable m_Table;
-	//private DefaultTableModel m_TableModel;
-	//private Map<Data, JLabel> m_NameLabels = new HashMap<Data, JLabel>();
-	//private Map<Data, JLabel> m_TypeLabels = new HashMap<Data, JLabel>();
-	//private Map<Data, Integer> m_RowIndex = new HashMap<Data, Integer>();
 	
 	public DataListPanel(JProbeCore core, DataTabPane tabPane){
 		super(new BorderLayout());
-		//super(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-		m_TabPane = tabPane;
-//		m_InnerPanel = new VerticleScrollPanel(new GridBagLayout());
-//		for(int i=0; i<Constants.DATALIST_COL_HEADERS.length; i++){
-//			String header = Constants.DATALIST_COL_HEADERS[i];
-//			GridBagConstraints gbc = new GridBagConstraints();
-//			gbc.gridx=i;
-//			gbc.fill = GridBagConstraints.HORIZONTAL;
-//			JLabel headerLabel = new JLabel(header);
-//			headerLabel.setHorizontalTextPosition(JLabel.CENTER);
-//			headerLabel.setBorder(BorderFactory.createLineBorder(Color.black));
-//			m_InnerPanel.add(headerLabel, gbc);
-//			m_InnerPanel.revalidate();
-//		}
-//		m_Row++;
-//		for(Data data : m_Core.getDataManager().getAllData()){
-//			this.add(data);
-//		}
-		m_Table = new DataListTable(core);
+		m_Table = new DataListTable(core, tabPane);
 		JScrollPane scroll = new JScrollPane(m_Table, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		TitledBorder border = BorderFactory.createTitledBorder("Data");
 		border.setTitleJustification(TitledBorder.CENTER);
 		this.setBorder(border);
 		this.add(scroll, BorderLayout.CENTER);
 		this.setMinimumSize(this.getPreferredSize());
-		//this.setViewportView(m_InnerPanel);
-		//this.add(m_InnerPanel, BorderLayout.NORTH);
 	}
-	
-//	private void add(Data data){
-//		JLabel name = new JLabel(m_Core.getDataManager().getDataName(data));
-//		JLabel type = new JLabel(data.getClass().getSimpleName());
-//		m_NameLabels.put(data, name);
-//		m_TypeLabels.put(data, type);
-//		GridBagConstraints gbc = new GridBagConstraints();
-//		gbc.gridy=m_Row;
-//		gbc.fill = GridBagConstraints.HORIZONTAL;
-//		gbc.gridx = 0;
-//		name.setBorder(BorderFactory.createLineBorder(Color.black));
-//		m_InnerPanel.add(name, gbc);
-//		gbc.gridx = 1;
-//		type.setBorder(BorderFactory.createLineBorder(Color.black));
-//		m_InnerPanel.add(type, gbc);
-//		m_Row++;
-//		m_InnerPanel.revalidate();
-//	}
 	
 	public void cleanup(){
 		m_Table.cleanup();
@@ -97,32 +41,5 @@ public class DataListPanel extends JPanel{
 		gbc.weightx = 0;
 		return gbc;
 	}
-
-//	@Override
-//	public void update(CoreEvent event) {
-//		if(event.type() == CoreEvent.Type.DATA_ADDED){
-//			Data added = event.getData();
-//			this.add(added);
-//		}
-//		if(event.type() == CoreEvent.Type.DATA_REMOVED){
-//			Data removed = event.getData();
-//			if(m_NameLabels.containsKey(removed)){
-//				m_InnerPanel.remove(m_NameLabels.get(removed));
-//				m_NameLabels.remove(removed);
-//			}
-//			if(m_TypeLabels.containsKey(removed)){
-//				m_InnerPanel.remove(m_TypeLabels.get(removed));
-//				m_TypeLabels.remove(removed);
-//			}
-//			m_InnerPanel.revalidate();
-//		}
-//		if(event.type() == CoreEvent.Type.DATA_NAME_CHANGE){
-//			Data changed = event.getData();
-//			if(m_NameLabels.containsKey(changed)){
-//				m_NameLabels.get(changed).setText(m_Core.getDataManager().getDataName(changed));
-//			}
-//			m_InnerPanel.revalidate();
-//		}
-//	}
 	
 }
--- src/plugins/dataviewer/gui/datalist/DataListTable.java
+++ src/plugins/dataviewer/gui/datalist/DataListTable.java
@@ -1,17 +1,21 @@
 package plugins.dataviewer.gui.datalist;
 
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 import javax.swing.JTable;
 import javax.swing.SwingUtilities;
 
 import plugins.dataviewer.gui.Constants;
+import plugins.dataviewer.gui.DataTabPane;
 import jprobe.services.JProbeCore;
 
-public class DataListTable extends JTable{
+public class DataListTable extends JTable implements MouseListener{
 	private static final long serialVersionUID = 1L;
 	
 	DataListModel m_Model;
 	
-	public DataListTable(JProbeCore core){
+	public DataListTable(JProbeCore core, DataTabPane tabPane){
 		super();
 		m_Model = new DataListModel(core, SwingUtilities.getWindowAncestor(this));
 		this.setModel(m_Model);
@@ -22,10 +26,37 @@ public class DataListTable extends JTable{
 		this.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
 		this.setPreferredScrollableViewportSize(this.getPreferredSize());
 		this.revalidate();
+		this.addMouseListener(this);
 	}
 	
 	public void cleanup(){
 		m_Model.cleanup();
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		//do nothing
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		//do nothing
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		//do nothing
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		//do nothing
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 	
