bdd165fde190dbb099ca50178be5c0bc333a8a32
tbepler <tbepler@gmail.com>
2014-05-16 18:29:19 -0400

    in the process of implementing DataArgsComponent to allow for multiple data objects to be selected for one argument

--- src/jprobe/services/functionRework/components/DataArgsComponent.java
+++ src/jprobe/services/functionRework/components/DataArgsComponent.java
@@ -1,17 +1,22 @@
 package jprobe.services.functionRework.components;
 
+import java.awt.Window;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
+import jprobe.services.CoreEvent;
+import jprobe.services.CoreListener;
 import jprobe.services.JProbeCore;
 import jprobe.services.data.Data;
 
-public class DataArgsComponent extends JPanel implements ValidNotifier, ItemListener{
+public class DataArgsComponent extends JPanel implements ValidNotifier, ItemListener, CoreListener{
 	private static final long serialVersionUID = 1L;
 	
 	public static interface DataValidFunction{
@@ -19,6 +24,9 @@ public class DataArgsComponent extends JPanel implements ValidNotifier, ItemList
 	}
 	
 	private final Collection<ValidListener> m_Listeners = new HashSet<ValidListener>();
+	
+	private final List<DataComboBox> m_DataComps = new ArrayList<DataComboBox>();
+	private final Collection<Data> m_SelectedData = new HashSet<Data>();
 	
 	private final JProbeCore m_Core;
 	private final int m_MinArgs;
@@ -29,18 +37,55 @@ public class DataArgsComponent extends JPanel implements ValidNotifier, ItemList
 	private boolean m_Valid;
 	
 	public DataArgsComponent(JProbeCore core, int minArgs, int maxArgs, boolean allowDuplicates, DataValidFunction validFunction){
-		
+		super();
+		m_Core = core;
+		m_Core.addCoreListener(this);
+		m_MinArgs = minArgs;
+		m_MaxArgs = maxArgs;
+		m_AllowDuplicates = allowDuplicates;
+		m_ValidFunction = validFunction;
+	}
+	
+	private void resizeWindow(){
+		Window ancestor = SwingUtilities.getWindowAncestor(this);
+		if(ancestor != null){
+			ancestor.pack();
+		}
+	}
+	
+	private boolean shouldAddData(Data d){
+		return m_AllowDuplicates || !m_SelectedData.contains(d);
+	}
+	
+	private void addDataComponent(){
+		DataComboBox comp = new DataComboBox(m_Core);
+		comp.addItemListener(this);
+		for(Data d : m_Core.getDataManager().getAllData()){
+			if(shouldAddData(d)){
+				comp.addData(d);
+			}
+		}
+		m_DataComps.add(comp);
+		this.add(comp);
+		this.resizeWindow();
 	}
 	
 	public List<Data> getDataArgs(){
-		//TODO
-		return null;
+		List<Data> data = new ArrayList<Data>();
+		for(DataComboBox box : m_DataComps){
+			data.add(box.getSelectedData());
+		}
+		return data;
 	}
 
 	@Override
 	public void itemStateChanged(ItemEvent e) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	protected boolean isValid(Data d){
+		return m_ValidFunction.isValid(d);
 	}
 	
 	@Override
@@ -62,6 +107,12 @@ public class DataArgsComponent extends JPanel implements ValidNotifier, ItemList
 	@Override
 	public void removeListener(ValidListener l) {
 		m_Listeners.remove(l);
+	}
+
+	@Override
+	public void update(CoreEvent event) {
+		// TODO Auto-generated method stub
+		
 	}
 
 	
--- src/jprobe/services/functionRework/components/DataComboBox.java
+++ src/jprobe/services/functionRework/components/DataComboBox.java
@@ -7,50 +7,25 @@ import java.util.Map;
 import javax.swing.JComboBox;
 import javax.swing.SwingUtilities;
 
-import jprobe.services.CoreEvent;
-import jprobe.services.CoreListener;
 import jprobe.services.JProbeCore;
 import jprobe.services.data.Data;
 
-public class DataComboBox extends JComboBox<String> implements CoreListener{
+public class DataComboBox extends JComboBox<String>{
 	private static final long serialVersionUID = 1L;
-	
-	public static interface DataValidFunction{
-		public boolean isValid(Data d);
-	}
 
 	public static final String DATACOMBOBOX_PROTOTYPE_DISPLAY = "SomeDataHere";
 	
 	private final JProbeCore m_Core;
-	private final DataValidFunction m_ValidFunction;
 	private final Map<String, Data> m_Displayed = new HashMap<String, Data>();
 	private final Map<Data, String> m_Data = new HashMap<Data, String>();
 	
-	public DataComboBox(DataValidFunction function, JProbeCore core){
+	public DataComboBox(JProbeCore core){
 		super();
 		this.setPrototypeDisplayValue(DATACOMBOBOX_PROTOTYPE_DISPLAY);
 		m_Core = core;
-		m_Core.addCoreListener(this);
-		m_ValidFunction = function;
-		for(Data d : m_Core.getDataManager().getAllData()){
-			if(isValid(d)){
-				String name = m_Core.getDataManager().getDataName(d);
-				m_Displayed.put(name, d);
-				m_Data.put(d, name);
-				this.addItem(name);
-			}
-		}
 	}
 	
-	public void cleanup(){
-		m_Core.removeCoreListener(this);
-	}
-	
-	private boolean isValid(Data d){
-		return m_ValidFunction.isValid(d);
-	}
-	
-	private void addData(Data d){
+	public void addData(Data d){
 		String name = m_Core.getDataManager().getDataName(d);
 		m_Displayed.put(name, d);
 		m_Data.put(d, name);
@@ -59,7 +34,7 @@ public class DataComboBox extends JComboBox<String> implements CoreListener{
 		//this.revalidate();
 	}
 	
-	private void removeData(Data d){
+	public void removeData(Data d){
 		String name = m_Data.get(d);
 		if(m_Displayed.containsKey(name)){
 			m_Displayed.remove(name);
@@ -77,7 +52,7 @@ public class DataComboBox extends JComboBox<String> implements CoreListener{
 		}
 	}
 	
-	private void rename(String oldName, String newName){
+	public void rename(String oldName, String newName){
 		if(m_Displayed.containsKey(oldName)){
 			Data changed = m_Displayed.get(oldName);
 			m_Data.put(changed, newName);
@@ -103,25 +78,6 @@ public class DataComboBox extends JComboBox<String> implements CoreListener{
 			if(this.getItemAt(i).equals(item)) return i;
 		}
 		return -1;
-	}
-
-	@Override
-	public void update(CoreEvent event) {
-		if(event.type() == CoreEvent.Type.DATA_ADDED){
-			Data added = event.getData();
-			if(this.isValid(added)){
-				this.addData(added);
-			}
-		}
-		if(event.type() == CoreEvent.Type.DATA_REMOVED){
-			Data removed = event.getData();
-			this.removeData(removed);
-		}
-		if(event.type() == CoreEvent.Type.DATA_NAME_CHANGE){
-			String oldName = event.getOldName();
-			String newName = event.getNewName();
-			this.rename(oldName, newName);
-		}
 	}
 	
 	public Data getSelectedData(){
