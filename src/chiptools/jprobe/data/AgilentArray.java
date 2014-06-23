7162eaf7947f697c980cc5be8ed20e2d13881e7c
tbepler <tbepler@gmail.com>
2014-06-23 14:35:05 -0400

    implemented AgilentArray and added indexing of AgilentProbe fields.

---
+++ src/chiptools/jprobe/data/AgilentArray.java
@@ -0,0 +1,69 @@
+package chiptools.jprobe.data;
+
+import java.util.List;
+
+import jprobe.services.data.AbstractFinalData;
+
+public class AgilentArray extends AbstractFinalData{
+	private static final long serialVersionUID = 1L;
+
+	public static final int NUM_COLS = 1 + AgilentProbe.NUM_ENTRIES;
+	public static final int NAME = 0;
+	
+	private final String m_Name;
+	private final List<AgilentProbe> m_Probes;
+	
+	protected AgilentArray(String name, List<AgilentProbe> probes) {
+		super(NUM_COLS, probes.size());
+		m_Name = name;
+		m_Probes = probes;
+	}
+
+	@Override
+	public void dispose() {
+		//do nothing;
+	}
+
+	@Override
+	public Class<?> getColumnClass(int col) {
+		return String.class;
+	}
+
+	@Override
+	public String getColumnName(int col) {
+		switch(col){
+		case NAME:
+			return "Name";
+		default:
+			return AgilentProbe.getEntryName(col + 1);
+		}
+	}
+	
+	public int size(){
+		return m_Probes.size();
+	}
+	
+	protected int getNumPlaces(){
+		return this.size() / 10 + 1;
+	}
+
+	protected String getName(int index){
+		String format = "%s_%-0"+this.getNumPlaces()+"s";
+		return String.format(format, m_Name, index+1);
+	}
+	
+	public String toString(int index){
+		return this.getName(index) + "\t" + m_Probes.get(index).toString(this.getNumPlaces());
+	}
+
+	@Override
+	public Object getValueAt(int row, int col) {
+		switch(col){
+		case NAME:
+			return this.getName(row);
+		default:
+			return m_Probes.get(row).getEntry(col - 1, this.getNumPlaces());
+		}
+	}
+
+}
--- src/chiptools/jprobe/data/AgilentProbe.java
+++ src/chiptools/jprobe/data/AgilentProbe.java
@@ -8,6 +8,36 @@ import util.genome.GenomicRegion;
 
 public class AgilentProbe implements Serializable{
 	private static final long serialVersionUID = 1L;
+	
+	public static final int NUM_ENTRIES = 6;
+	public static final int SEQ = 0;
+	public static final int CATEGORY = 1;
+	public static final int BLANK_BLANK = 2;
+	public static final int BLANK = 3;
+	public static final int CATEGORY_SECOND = 4;
+	public static final int REGION = 5;
+	
+	public static final String NA_NA = "Na|Na";
+	public static final String NA = "Na";
+	
+	public static String getEntryName(int entry){
+		switch(entry){
+		case SEQ:
+			return "Sequence";
+		case CATEGORY:
+			return "Category";
+		case BLANK_BLANK:
+			return "Na";
+		case BLANK:
+			return "Na";
+		case CATEGORY_SECOND:
+			return "Category";
+		case REGION:
+			return "Region";
+		default:
+			return "";
+		}
+	}
 
 	private final String m_Seq;
 	private final String m_Category;
@@ -25,13 +55,44 @@ public class AgilentProbe implements Serializable{
 		m_Hash = this.generateHash();
 	}
 	
+	public int getNumEntries(){
+		return NUM_ENTRIES;
+	}
+	
+	public String getEntry(int e, int numPlaces){
+		switch(e){
+		case SEQ:
+			return m_Seq;
+		case CATEGORY:
+			return this.getCategoryAndInfo(numPlaces);
+		case BLANK_BLANK:
+			return NA_NA;
+		case BLANK:
+			return NA;
+		case CATEGORY_SECOND:
+			return this.getCategoryAndInfo(numPlaces);
+		case REGION:
+			return m_Region.toString();
+		default:
+			return null;
+		}
+	}
+	
 	protected int generateHash(){
 		return new HashCodeBuilder(5, 117).append(m_Seq).append(m_Category).append(m_CategoryNum).append(m_Info).append(m_Region).toHashCode();
 	}
 	
+	public String getNaNa(){
+		return NA_NA;
+	}
+	
+	public String getNa(){
+		return NA;
+	}
+	
 	public String toString(int numPlaces){
 		String catAndInfo = this.getCategoryAndInfo(numPlaces);
-		return m_Seq + "\t" + catAndInfo + "\tNa|Na\tNa\t" + catAndInfo + "\t" + m_Region; 
+		return m_Seq + "\t" + catAndInfo + "\t"+NA_NA+"\t"+NA+"\t" + catAndInfo + "\t" + m_Region; 
 	}
 	
 	public String getCategoryAndInfo(int numPlaces){
