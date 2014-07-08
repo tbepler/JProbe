066eecaae1a58a2ddd0390b9786599b823a0a08e
tbepler <tbepler@gmail.com>
2014-07-08 12:18:58 -0400

    Warning no longer subclasses Exception, instead it can wrap a Throwable object cause. Warning and WarningHandler now have a generic enumeration for defining Warning types.

--- src/util/warning/Warning.java
+++ src/util/warning/Warning.java
@@ -1,20 +1,24 @@
 package util.warning;
 
+import java.io.Serializable;
+
 /**
  * This interface represents a warning that can be dispatched to query a caller
  * about whether the executing procedure should {@link Reply#PROCEED} or {@link Reply#CANCEL}.
- * A description of the warning is provided by the {@link #getMessage()} method.
+ * The type of the warning is provided by the {@link #getType()} method.
  * <P>
- * This calls extends {@link Exception} in order to easily link a {@link Throwable} cause
- * to this Warning and in order to provide {@link #getStackTrace()} and {@link #printStackTrace()}
- * methods if desired. This also means that Warnings should be used sparingly, as frequent Exception
- * construction can be expensive.
+ * Objects of this class may provide a {@link Throwable} cause accessed through the {@link Warning#getCause()}
+ * method if desired.
  * 
  * @author Tristan Bepler
  *
+ * @param E - Enumeration containing the possible types of this Warning
  */
-public class Warning extends Exception{
+public class Warning<E extends Enum<E>> implements Serializable{
 	private static final long serialVersionUID = 1L;
+	
+	private final E m_Type;
+	private final Throwable m_Cause;
 	
 	/**
 	 * Constructs a new {@link Warning} with the given message, which
@@ -22,20 +26,38 @@ public class Warning extends Exception{
 	 * of this {@link Warning} is set to null.
 	 * @param message - description of this {@link Warning}
 	 */
-	public Warning(String message){
-		super(message);
+	public Warning(E type){
+		this(type, null);
 	}
 	
 	/**
-	 * Constructs a new {@link Warning} with the given message and cause.
-	 * The message can be returned with a call to {@link Warning#getMessage()} and
+	 * Constructs a new {@link Warning} with the given type and cause.
+	 * The type can be returned with a call to {@link Warning#getType()} and
 	 * the cause can be retrieved with {@link Warning#getCause()}.
 	 * @param message - description of this {@link Warning}
 	 * @param cause - cause of this {@link Warning}
 	 * @see Exception#Exception(String, Throwable)
 	 */
-	public Warning(String message, Throwable cause) {
-		super(message, cause);
+	public Warning(E type, Throwable cause) {
+		m_Type = type;
+		m_Cause = cause;
+	}
+	
+	/**
+	 * Returns the type of this warning.
+	 * @return
+	 */
+	public E getType(){
+		return m_Type;
+	}
+	
+	/**
+	 * Returns the {@link Throwable} that caused this warning. May be null
+	 * if no cause is specified.
+	 * @return
+	 */
+	public Throwable getCause(){
+		return m_Cause;
 	}
 	
 	
--- src/util/warning/WarningHandler.java
+++ src/util/warning/WarningHandler.java
@@ -3,22 +3,26 @@ package util.warning;
 /**
  * This interface defines the {@link #handle(Warning)} method which can be
  * used by an executing procedure to alert the caller of an {@link Warning},
- * which allows the caller to specify if the procedure should {@link Warning#proceed()}
- * or {@link Warning#cancel()}.
+ * which allows the caller to specify if the procedure should {@link Reply#PROCEED}
+ * or {@link Reply#CANCEL}.
  * 
  * @author Tristan Bepler
+ * 
+ * @param E - Enumeration containing the types of {@link Warning}s this WarningHandler
+ * can receive.
  *
  */
-public interface WarningHandler {
+public interface WarningHandler<E extends Enum<E>> {
 	
 	/**
 	 * Alerts this {@link WarningHandler} that the given {@link Warning} has occurred.
-	 * The {@link Warning#getMessage()} method can be used to retrieve a description
-	 * of the warning.
-	 * @param warning - {@link Warning} object being reported.
-	 * @return a {@link Reply} indicating whether the {@link Warning} producer should
+	 * The {@link Warning#getType()} method can be used to retrieve the type of the Warning.
+	 * The {@link Warning#getCause()} method can be used to retrieve the {@link Throwable}
+	 * cause of this Warning, if there is one.
+	 * @param warning - Warning object being reported.
+	 * @return a {@link Reply} indicating whether the Warning producer should
 	 * {@link Reply#PROCEED} with or {@link Reply#CANCEL} execution.
 	 */
-	public Reply handle(Warning warning);
+	public Reply handle(Warning<E> warning);
 	
 }
