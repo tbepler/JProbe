b8241f932bac51e01e6ba1b988e4ba557164521e
tbepler <tbepler@gmail.com>
2014-07-10 16:25:23 -0400

    Composable function structure appears to work well. Deep nesting of 2 parameter functions seems to be able to go about 2000 functions deep along with the 2002 parameter defining functions without a stack overflow.

--- src/jprobe/framework/model/Function.java
+++ src/jprobe/framework/model/Function.java
@@ -1,12 +1,6 @@
 package jprobe.framework.model;
 
-import java.io.Serializable;
-
-public interface Function<R> extends Serializable{
-	
-	public Parameter<?>[] getParameters();
-	
-	public Class<? extends R> returnType();
+public interface Function<R> extends Procedure<R>{
 	
 	/*
 	 * One parameter - one function
@@ -15,9 +9,6 @@ public interface Function<R> extends Serializable{
 	public <T> Function<R> putArgument(int paramIndex, Function<T> arg) throws TypeMismatchException;
 	public <T> Function<R> putArgument(int paramIndex, Value<T> arg) throws TypeMismatchException;
 	public <T> Function<R> putArgument(int paramIndex, T arg) throws TypeMismatchException;
-	
-	public R invoke(Value<?> ... args)
-			throws IllegalArgumentException, TypeMismatchException, InvocationException;
 	
 
 }
--- src/jprobe/system/model/test/FunctionTest.java
+++ src/jprobe/system/model/test/FunctionTest.java
@@ -3,6 +3,7 @@ package jprobe.system.model.test;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.concurrent.ExecutionException;
 
 import jprobe.framework.model.Function;
@@ -52,7 +53,7 @@ public class FunctionTest extends junit.framework.TestCase{
 	private static final FunctionFactory FACTORY = new FunctionFactoryImpl();
 	private static final Add ADD_PROCEDURE = new Add();
 	
-	private static int sum(int[] vals){
+	private static int sum(int ... vals){
 		int sum = 0;
 		for(int i : vals){
 			sum += i;
@@ -86,16 +87,84 @@ public class FunctionTest extends junit.framework.TestCase{
 		
 	}
 	
-	/*
-	public void testNested() throws MissingArgumentsException, ExecutionException{
+	
+	public void testNested() throws TypeMismatchException, IllegalArgumentException, InvocationException {
+		
+		Function<Integer> add = FACTORY.newFunction(ADD_PROCEDURE);
 		
 		int[] vals = new int[]{5, 10, 23, 12, 56, 37};
+		Value<?>[] args = new Value<?>[vals.length];
+		for(int i=0; i<vals.length; i++){
+			args[i] = new FixedValue<Integer>(vals[i]);
+		}
 		
-		int sum = 0;
 		int i = 0;
+		Function<Integer> call1 = add.putArgument(0, add);
+		int sum3 = sum(vals[0], vals[1], vals[2]);
+		assertEquals(sum3, (int) call1.invoke(args[0], args[1], args[2]));
 		
+		Function<Integer> call2 = call1.putArgument(0, vals[i++]);
+		assertEquals(sum3, (int) call2.invoke(args[1], args[2]));
 		
+		Function<Integer> call3 = call2.putArgument(0, vals[i++]);
+		assertEquals(sum3, (int) call3.invoke(args[2]));
+		
+		Function<Integer> call4 = call3.putArgument(0, add);
+		int sum4 = sum(sum3, vals[3]);
+		assertEquals(sum4, (int) call4.invoke(args[2], args[3]));
+		
+		Function<Integer> call5 = call4.putArgument(0, add);
+		int sum5 = sum(sum4, vals[4]);
+		assertEquals(sum5, (int) call5.invoke(args[2], args[3], args[4]));
+		
+		Function<Integer> call6 = call5.putArgument(0, vals[i++]);
+		assertEquals(sum5, (int) call6.invoke(args[3], args[4]));
+		
+		Function<Integer> call7 = call6.putArgument(0, vals[i++]);
+		assertEquals(sum5, (int) call7.invoke(args[4]));
+		
+		Function<Integer> call8 = call7.putArgument(0, add);
+		int sum6 = sum(sum5, vals[5]);
+		assertEquals(sum6, (int) call8.invoke(args[4], args[5]));
+		
+		Function<Integer> call9 = call8.putArgument(0, vals[i++]);
+		assertEquals(sum6, (int) call9.invoke(args[5]));
+		
+		Function<Integer> call10 = call9.putArgument(0, vals[i++]);
+		assertEquals(sum6, (int) call10.invoke(null));
+		
+		Function<Integer> nested5 = call5.putArgument(1, call5);
+		int nested5Sum = sum(vals[0], vals[1], vals[0], vals[1]);
+		assertEquals(sum(nested5Sum, vals[2], vals[2], vals[3], vals[3], vals[4]),
+				(int) nested5.invoke(args[2], args[2], args[3], args[3], args[4]));
 		
 	}
-	*/
+	
+	public void testDeepNesting() throws TypeMismatchException, IllegalArgumentException, InvocationException{
+		int seed = 13;
+		Random r = new Random(seed);
+		int depth = 1500;
+		int params = 2 + depth;
+		Function<Integer> add = FACTORY.newFunction(ADD_PROCEDURE);
+		Function<Integer> nested = add;
+		for(int i=0; i<depth; i++){
+			int index = r.nextInt(nested.getParameters().length);
+			nested = nested.putArgument(index, add);
+		}
+		assertEquals(params, nested.getParameters().length);
+		
+		int sum = 0;
+		for(int i=0; i<params; i++){
+			sum += i + 1;
+			nested = nested.putArgument(0, i + 1);
+		}
+		
+		assertEquals(sum, (int) nested.invoke(null));
+		
+	}
+	
+	
+	
+	
+	
 }
