18234ea2187bf27ca6a17e06887a59b5ce786409
tbepler <tbepler@gmail.com>
2014-07-18 18:07:35 -0400

    Implemented the parser's closure and goto methods. Now need to implement the table construction method.

--- src/jprobe/framework/model/compiler/Parser.java
+++ src/jprobe/framework/model/compiler/Parser.java
@@ -1,5 +1,6 @@
 package jprobe.framework.model.compiler;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Deque;
 import java.util.HashSet;
@@ -10,6 +11,109 @@ import java.util.Set;
 import jprobe.framework.model.compiler.lexer.Token;
 
 public class Parser<S> {
+	
+	private static class Item<S>{
+		
+		private final S lhs;
+		private final S[] rhs;
+		private final int index;
+		private final int hash;
+		
+		public Item(Production<S> prod){
+			this(prod.leftHandSide(), prod.rightHandSide());
+		}
+		
+		public Item(S lhs, S ... rhs){
+			this(0, lhs, rhs);
+		}
+		
+		private Item(int index, S lhs, S ... rhs){
+			this.index = index;
+			this.lhs = lhs;
+			this.rhs = rhs;
+			hash = Arrays.hashCode(new Object[]{index, lhs, rhs});
+		}
+		
+		public Item<S> increment(){
+			return new Item<S>(index+1, lhs, rhs);
+		}
+		
+		public int index(){
+			return index;
+		}
+		
+		public boolean hasNext(){
+			return index < rhs.length;
+		}
+		
+		public S nextSymbol(){
+			return rhs[index];
+		}
+		
+		public S leftHandSide(){
+			return lhs;
+		}
+		
+		@Override
+		public boolean equals(Object o){
+			if(o == null) return false;
+			if(o == this) return true;
+			if(o instanceof Item){
+				Item<?> i = (Item<?>) o;
+				return index == i.index && lhs.equals(i.lhs) && Arrays.equals(rhs, i.rhs);
+			}
+			return false;
+		}
+		
+		@Override
+		public int hashCode(){
+			return hash;
+		}
+		
+	}
+	
+	private static class State<S>{
+		
+		private final Set<Item<S>> items;
+		private final int hash;
+		
+		public State(Set<Item<S>> items){
+			this.items = new HashSet<Item<S>>(items);
+			hash = Arrays.hashCode(items.toArray());
+		}
+		
+		public Set<Item<S>> asSet(){
+			return Collections.unmodifiableSet(items);
+		}
+		
+		@Override
+		public int hashCode(){
+			return hash;
+		}
+		
+		@Override
+		public boolean equals(Object o){
+			if(o == null) return false;
+			if(o == this) return true;
+			if(o instanceof State){
+				State<?> s = (State<?>) o;
+				if(items.size() == s.items.size()){
+					for(Item<S> item : items){
+						if(!s.items.contains(item)){
+							return false;
+						}
+					}
+					return true;
+				}
+			}
+			return false;
+		}
+		
+	}
+	
+	private static class Action{
+		
+	}
 	
 	private final Grammar<S> m_Grammar;
 	private final Set<S> m_Nullable;
@@ -138,6 +242,40 @@ public class Parser<S> {
 	
 	public void parse(List<Token> tokens){
 		
+	}
+	
+	private Map<State<S>,Map<S,Action>> constructParseTable(){
+		
+	}
+	
+	private Set<Item<S>> gotoo(Set<Item<S>> items, S symbol){
+		Set<Item<S>> shifted = new HashSet<Item<S>>();
+		for(Item<S> item : items){
+			if(item.hasNext() && symbol.equals(item.nextSymbol())){
+				shifted.add(item.increment());
+			}
+		}
+		return this.closure(shifted);
+	}
+	
+	private Set<Item<S>> closure(Set<Item<S>> items){
+		S next;
+		boolean changed;
+		do{
+			changed = false;
+			Set<Item<S>> copy = new HashSet<Item<S>>(items);
+			for(Item<S> item : items){
+				if(item.hasNext()){
+					next = item.nextSymbol();
+					for(Production<S> p : m_Grammar.getProductions(next)){
+						Item<S> newItem = new Item<S>(p);
+						changed = copy.add(newItem) || changed;
+					}
+				}
+			}
+			items = copy;
+		}while(changed);
+		return items;
 	}
 	
 	private static String generateBadParseMessage(List<Token> tokens, Deque<Element> stack){
